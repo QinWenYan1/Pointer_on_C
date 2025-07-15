cb84fb770599b5e320c260889f02973232acc571
Qinwen Yan <2721817y@student.gla.ac.uk>
2025-07-15 21:41:13 +0800

    update note and answer

--- chapter01/exercise/coding_2.c
+++ chapter01/exercise/coding_2.c
@@ -12,22 +12,19 @@ int main(void){
     *  we read the several lines of code
     *  if the user inputs nothing, the program will terminate
     */
-    while((ch = getchar()) != EOF){
+    while( ( ch = getchar() ) != EOF){
 
         /* the number printed for line and premission has. been used*/
-        if(flag){
+        if( flag ){
             printf("%d: ", num++);
             flag = 0; 
         }
         
-
         /*print the character*/
-        printf("%c", ch); 
+        printf( "%c", ch ); 
         
         /* we get premission of printing number if change the line */
-        if (ch == '\n'){flag = 1;}
-
-
+        if ( ch == '\n' ){ flag = 1; }
         
     }
 
--- chapter01/exercise/coding_3.c
+++ chapter01/exercise/coding_3.c
@@ -4,11 +4,12 @@ int main(void) {
     int ch;  // 必须是 int 才能判断 EOF
     signed char checksum = -1;
 
-    while ((ch = getchar()) != EOF) {   
+    while ( ( ch = getchar() ) != EOF) {   
         // 这里发生溢出就自动 wrap around              
         checksum = checksum + ch;      // 累加 checksum
     }
 
-    printf("%d\n", (int)checksum);     // 打印 checksum 并换行
+    printf( "%d\n", (int)checksum ); // 打印 checksum 并换行
+    
     return 0;
 }
